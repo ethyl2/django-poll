40119816b5b99adc8120ddcdf45031cf8be1ddbb
Heather Nuffer <zzzuzz@gmail.com>
2020-04-25 20:13:24 -0600

    Adds more functionality and styling for displaying polls and lets admin adds choices in question admin page

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,27 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    # Use admin.StackedInline above if you want the inputs to be displayed in a stack
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                {'fields': ['title']}),
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': [
+         'pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('title', 'question_text',
+                    'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+# Below isn't needed anymore with ChoiceInline added
+# admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -15,7 +15,14 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        # Original code that didn't take future time into account:
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
